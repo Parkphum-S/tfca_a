f96d587638ea5e1e2bceeea8969317e7fbe2b02d
Parkphum Srilerchan <84962350+Parkphum-S@users.noreply.github.com>
2022-12-06 23:03:29 +0700

    home commit

--- lib/views/checkPlanA_ui.dart
+++ lib/views/checkPlanA_ui.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:tfca_a/views/addTable_ui.dart';
 import 'package:tfca_a/views/checkA_ui.dart';
 
 class CheckPlanAUI extends StatefulWidget {
@@ -23,7 +24,7 @@ class _CheckPlanAUIState extends State<CheckPlanAUI> {
             Column(
               children: [
                 Text(
-                  "ตารางเรียน",
+                  "ตารางสอน",
                   style: TextStyle(
                     color: Color.fromARGB(255, 28, 45, 64),
                     fontSize: 20,
@@ -32,7 +33,7 @@ class _CheckPlanAUIState extends State<CheckPlanAUI> {
                   ),
                 ),
                 Text(
-                  "ตารางเรียนทั้งหมด",
+                  "รายการที่ทำการสอนทั้งหมด",
                   style: TextStyle(
                     color: Color.fromARGB(255, 28, 45, 64),
                     fontSize: 20,
@@ -648,7 +649,38 @@ class _CheckPlanAUIState extends State<CheckPlanAUI> {
               ),
             ),
             SizedBox(
-              height: 20.0,
+              height: 30.0,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 35.0, right: 35.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AddTableUI(),
+                          fullscreenDialog: true));
+                },
+                child: Text(
+                  'เพิ่มตารางสอน',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontFamily: 'Kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 78, 121, 240),
+                  minimumSize: const Size.fromHeight(50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
             ),
           ],
         ),
